68a6f0dabd26d04e4932b7f0d334aff098ecff15
Thomas Papanikolaou <thomas_papanikolaou@hotmail.com>
2022-09-04 00:48:32 +0200

    Progression FAQ

--- projet_fil_rouge/src/views/FAQ/FAQ2TEST.jsx
+++ projet_fil_rouge/src/views/FAQ/FAQ2TEST.jsx
@@ -9,84 +9,182 @@ import Card from 'react-bootstrap/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
+// function Example() {
+//     return (
+//         <Accordion defaultActiveKey="0">
+//             <Card>
+//                 <Card.Header>
+//                     <CustomToggle eventKey="0">Netflix, qu'est-ce que c'est ?
+//                         {/* <FontAwesomeIcon icon={faPlus}/> */}
+//                     </CustomToggle>
+//                 </Card.Header>
+//                 <Accordion.Collapse eventKey="0">
+//                     <Card.Body><p>Netflix est un service de streaming qui propose une vaste sélection de séries TV, films, animes, documentaires et autres programmes primés sur des milliers d'appareils connectés à Internet.</p><br />
+//                         <p>Regardez tout ce que vous voulez, quand vous voulez, sans publicité et à un tarif mensuel très attractif. Découvrez de nouveaux films et séries TV chaque semaine, il y en a pour tous les goûts !</p></Card.Body>
+//                 </Accordion.Collapse>
+//             </Card>
+//             <Card>
+//                 <Card.Header>
+//                     <CustomToggle eventKey="1">Combien coûte Netflix ? </CustomToggle>
+//                 </Card.Header>
+//                 <Accordion.Collapse eventKey="1">
+//                     <Card.Body>Regardez Netflix sur votre smartphone, tablette, Smart TV, ordinateur ou appareil de streaming, le tout pour un tarif mensuel fixe. Les forfaits vont de 8,99 € à 17,99 € par mois. Pas de contrat ni de frais supplémentaires.</Card.Body>
+//                 </Accordion.Collapse>
+//             </Card>
+//             <Card>
+//                 <Card.Header>
+//                     <CustomToggle eventKey="2">Où puis-je regarder Netflix ? </CustomToggle>
+//                 </Card.Header>
+//                 <Accordion.Collapse eventKey="2">
+//                     <Card.Body>Netflix, c'est où vous voulez, quand vous voulez. Connectez-vous à votre compte pour regarder Netflix en ligne sur netflix.com depuis votre ordinateur ou tout appareil connecté à Internet avec l'application Netflix, comme les Smart TV, smartphones, tablettes, lecteurs de streaming et consoles de jeu.
+
+//                         Vous pouvez aussi télécharger vos séries préférées avec l'application iOS, Android ou Windows 10. Téléchargez des titres pour les regarder sur votre appareil mobile, même sans connexion Internet. Emportez Netflix partout avec vous.</Card.Body>
+//                 </Accordion.Collapse>
+//             </Card>
+//             <Card>
+//                 <Card.Header>
+//                     <CustomToggle eventKey="3">Comment puis-je annuler mon forfait ?</CustomToggle>
+//                 </Card.Header>
+//                 <Accordion.Collapse eventKey="3">
+//                     <Card.Body>Netflix offre une grande souplesse. Pas de contrat compliqué. Sans engagement. Deux clics suffisent pour annuler votre compte en ligne. Pas de frais d'annulation : ouvrez ou fermez votre compte à tout moment.</Card.Body>
+//                 </Accordion.Collapse>
+//             </Card>
+//             <Card>
+//                 <Card.Header>
+//                     <CustomToggle eventKey="4">Que puis-je regarder sur Netflix ? </CustomToggle>
+//                 </Card.Header>
+//                 <Accordion.Collapse eventKey="4">
+//                     <Card.Body>Netflix propose un vaste catalogue comprenant notamment des longs métrages, des documentaires, des séries TV, des animes et des programmes originaux Netflix primés. Regardez Netflix à volonté, quand vous le voulez.</Card.Body>
+//                 </Accordion.Collapse>
+//             </Card>
+//             <Card>
+//                 <Card.Header>
+//                     <CustomToggle eventKey="5">Est-ce que Netflix est adapté aux enfants ? </CustomToggle>
+//                 </Card.Header>
+//                 <Accordion.Collapse eventKey="5">
+//                     <Card.Body>Netflix Jeunesse est inclus dans votre abonnement et offre un meilleur contrôle aux parents, ainsi qu'un espace dédié aux enfants, avec des films et des séries destinés à toute la famille.
+
+//                         Les profils Enfants comportent des fonctionnalités de contrôle parental avec code PIN permettant de modifier la catégorie d'âge des contenus que vos enfants peuvent regarder et de bloquer des titres spécifiques.</Card.Body>
+//                 </Accordion.Collapse>
+//             </Card>
+
+//         </Accordion>
+//     );
+// };
+
+
+
+
 function Example() {
     return (
-        <Accordion defaultActiveKey="0">
-            <Card>
-                <Card.Header>
-                    <CustomToggle eventKey="0">Netflix, qu'est-ce que c'est ?
-                        {/* <FontAwesomeIcon icon={faPlus}/> */}
-                    </CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                    <Card.Body><p>Netflix est un service de streaming qui propose une vaste sélection de séries TV, films, animes, documentaires et autres programmes primés sur des milliers d'appareils connectés à Internet.</p><br />
-                        <p>Regardez tout ce que vous voulez, quand vous voulez, sans publicité et à un tarif mensuel très attractif. Découvrez de nouveaux films et séries TV chaque semaine, il y en a pour tous les goûts !</p></Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            <Card>
-                <Card.Header>
-                    <CustomToggle eventKey="1">Combien coûte Netflix ? </CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                    <Card.Body>Regardez Netflix sur votre smartphone, tablette, Smart TV, ordinateur ou appareil de streaming, le tout pour un tarif mensuel fixe. Les forfaits vont de 8,99 € à 17,99 € par mois. Pas de contrat ni de frais supplémentaires.</Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            <Card>
-                <Card.Header>
-                    <CustomToggle eventKey="2">Où puis-je regarder Netflix ? </CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="2">
-                    <Card.Body>Netflix, c'est où vous voulez, quand vous voulez. Connectez-vous à votre compte pour regarder Netflix en ligne sur netflix.com depuis votre ordinateur ou tout appareil connecté à Internet avec l'application Netflix, comme les Smart TV, smartphones, tablettes, lecteurs de streaming et consoles de jeu.
+        <Card>
+            <Card.Header>
+                <button className='accordion'>Netflix, qu'est-ce que c'est ?</button>
+                <div className='panel'>
+                    <p>Netflix est un service de streaming qui propose une vaste sélection de séries, films, animes, documentaires et autres programmes primés sur des milliers d'appareils connectés à Internet.
 
-                        Vous pouvez aussi télécharger vos séries préférées avec l'application iOS, Android ou Windows 10. Téléchargez des titres pour les regarder sur votre appareil mobile, même sans connexion Internet. Emportez Netflix partout avec vous.</Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            <Card>
-                <Card.Header>
-                    <CustomToggle eventKey="3">Comment puis-je annuler mon forfait ?</CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="3">
-                    <Card.Body>Netflix offre une grande souplesse. Pas de contrat compliqué. Sans engagement. Deux clics suffisent pour annuler votre compte en ligne. Pas de frais d'annulation : ouvrez ou fermez votre compte à tout moment.</Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            <Card>
-                <Card.Header>
-                    <CustomToggle eventKey="4">Que puis-je regarder sur Netflix ? </CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="4">
-                    <Card.Body>Netflix propose un vaste catalogue comprenant notamment des longs métrages, des documentaires, des séries TV, des animes et des programmes originaux Netflix primés. Regardez Netflix à volonté, quand vous le voulez.</Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            <Card>
-                <Card.Header>
-                    <CustomToggle eventKey="5">Est-ce que Netflix est adapté aux enfants ? </CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="5">
-                    <Card.Body>Netflix Jeunesse est inclus dans votre abonnement et offre un meilleur contrôle aux parents, ainsi qu'un espace dédié aux enfants, avec des films et des séries destinés à toute la famille.
+                        Regardez tout ce que vous voulez, quand vous voulez, sans publicité et à un tarif mensuel très attractif. Découvrez de nouveaux films et séries chaque semaine, il y en a pour tous les goûts !</p>
+                </div>
 
-                        Les profils Enfants comportent des fonctionnalités de contrôle parental avec code PIN permettant de modifier la catégorie d'âge des contenus que vos enfants peuvent regarder et de bloquer des titres spécifiques.</Card.Body>
-                </Accordion.Collapse>
-            </Card>
+                <button className='accordion'>Combien coûte Netflix ?</button>
+                <div className='panel'>
+                    <p>Regardez Netflix sur votre smartphone, tablette, Smart TV, ordinateur ou appareil de streaming, le tout pour un tarif mensuel fixe. Les forfaits vont de 8,99 € à 17,99 € par mois. Pas de contrat ni de frais supplémentaires.</p>
+                </div>
 
-        </Accordion>
+                <button className='accordion'>Où puis-je regarder Netflix ?</button>
+                <div className='panel'>
+                    <p>Netflix, c'est où vous voulez, quand vous voulez. Connectez-vous à votre compte pour regarder Netflix en ligne sur netflix.com depuis votre ordinateur ou tout appareil connecté à Internet avec l'application Netflix, comme les Smart TV, smartphones, tablettes, lecteurs de streaming et consoles de jeu.
+
+                        Vous pouvez aussi télécharger vos séries préférées avec l'application iOS, Android ou Windows 10. Téléchargez des titres pour les regarder sur votre appareil mobile, même sans connexion Internet. Emportez Netflix partout avec vous.</p>
+                </div>
+
+                <button className='accordion'>Comment puis-je annuler mon forfait ?</button>
+                <div className='panel'>
+                    <p>Netflix offre une grande souplesse. Pas de contrat compliqué. Sans engagement. Deux clics suffisent pour annuler votre compte en ligne. Pas de frais d'annulation : ouvrez ou fermez votre compte à tout moment.</p>
+                </div>
+
+                <button className='accordion'>Que puis-je regarder sur Netflix ?</button>
+                <div className='panel'>
+                    <p>Netflix propose un vaste catalogue comprenant notamment des longs métrages, des documentaires, des séries, des animes et des programmes originaux Netflix primés. Regardez Netflix à volonté, quand vous le voulez.</p>
+                </div>
+
+                {/* <button className='accordion'>Est-ce que Netflix est adapté aux enfants ?</button>
+                <div className='panel'>
+                    <p>Netflix Jeunesse est inclus dans votre abonnement et offre un meilleur contrôle aux parents, ainsi qu'un espace dédié aux enfants, avec des films et des séries destinés à toute la famille.
+
+                        Les profils Jeunesse comportent des fonctionnalités de contrôle parental avec code PIN permettant de modifier la catégorie d'âge des contenus que vos enfants peuvent regarder et de bloquer des titres spécifiques.</p>
+                </div> */}
+            </Card.Header>
+        </Card>
+
     );
 };
 
 
-function CustomToggle({ children, eventKey }) {
-    const decoratedOnClick = useAccordionButton(eventKey, () =>
-        console.log('totally custom!'),
-    );
 
-    return (
-        <span
-            type="button"
-            onClick={decoratedOnClick}
-        >
-            {children}
-        </span>
-    );
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function CustomToggle({ children, eventKey }) {
+//     const decoratedOnClick = useAccordionButton(eventKey, () =>
+//         console.log('totally custom!'),
+//     );
+
+//     return (
+//         <span
+//             type="button"
+//             onClick={decoratedOnClick}
+//         >
+//             {children}
+//         </span>
+//     );
+// }
 
 export default Example;
 
