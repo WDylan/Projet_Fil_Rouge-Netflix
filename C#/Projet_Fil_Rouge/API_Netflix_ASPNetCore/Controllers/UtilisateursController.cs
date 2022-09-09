00224e2321e7b1c714b2ac9d94aefc6d24151060
Thomas Papanikolaou <thomas_papanikolaou@hotmail.com>
2022-09-09 11:11:15 +0200

    Debut controller utilisateurs

---
+++ C#/Projet_Fil_Rouge/API_Netflix_ASPNetCore/Controllers/UtilisateursController.cs
@@ -0,0 +1,92 @@
+﻿using API_Netflix_ASPNetCore.Models.Classes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Netflix_ASPNetCore.Controllers
+{
+    public class UtilisateursController : Controller
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        // GET: api/<FilmsController>
+        [HttpGet]
+        //public IEnumerable<Utilisateurs> Get()
+        //{
+        //    List<Utilisateurs> utilisateursList = new();
+        //    utilisateursList = Utilisateurs.GetAll();
+        //    return utilisateursList;
+        //}
+
+        // GET: api/<UtilisateursController>/5
+        [HttpGet("{id}")]
+        public Utilisateurs Get(int id)
+        {
+            Utilisateurs utilisateur = new();
+            utilisateur = utilisateur.Get(id).Item2;
+            return utilisateur;
+        }
+
+        // GET: UtilisateursController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UtilisateursController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UtilisateursController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UtilisateursController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UtilisateursController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UtilisateursController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ C#/Projet_Fil_Rouge/API_Netflix_ASPNetCore/Models/Classes/Utilisateurs.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace API_Netflix_ASPNetCore.Models.Classes
+{
+    public class Utilisateurs
+    {
+        private int idUtilisateur;
+        private string nom;
+        private string prenom;
+        private string email;
+        private string motdepasse;
+        private string statut;
+        private string _request;
+        private SqlCommand _command;
+        private SqlConnection _connection;
+        private SqlDataReader _reader;
+        private static List<Utilisateurs> utilisateur;
+
+        public int IdUtilisateur { get => idUtilisateur; set => idUtilisateur = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Email { get => email; set => email = value; }
+        public string Motdepasse { get => motdepasse; set => motdepasse = value; }
+        public string Statut { get => statut; set => statut = value; }
+        public string Request { get => _request; set => _request = value; }
+        public static List<Utilisateurs> Utilisateur { get => utilisateur; set => utilisateur = value; }
+
+
+        public (bool, Utilisateurs) Get(int id)
+        {
+            Utilisateurs utilisateur = null;
+            bool found = false;
+            _connection = Connection.New;
+            _request = "SELECT uti.nom, uti.prenom, uti.email, uti.motdepasse, uti.statut" +
+                "FROM UTILISATEURS AS uti";
+            _command = new SqlCommand(_request, _connection);
+            _command.Parameters.Add(new SqlParameter("@IdUtilisateur", id));
+            _connection.Open();
+            _reader = _command.ExecuteReader();
+            if (_reader.Read())
+            {
+                utilisateur = new Utilisateurs()
+                {
+                    IdUtilisateur = _reader.GetInt32(0),
+                    Nom = _reader.GetString(1),
+                    Prenom = _reader.GetString(2),
+                    Email = _reader.GetString(3),
+                    Motdepasse = _reader.GetString(4),
+                    Statut = _reader.GetString(5),
+                };
+                found = true;
+            }
+            _reader.Close();
+            _command.Dispose();
+            _connection.Close();
+            return (found, utilisateur);
+        }
+
+
+        public static List<Utilisateurs> GetAll()
+        {
+            List<Utilisateurs> utilisateurs = new List<Utilisateurs>();
+            SqlConnection connection = Connection.New;
+            string request = "SELECT uti.nom, uti.prenom, uti.email, uti.motdepasse, uti.statut" +
+                "FROM UTILISATEURS AS uti";
+
+            SqlCommand command = new SqlCommand(request, connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Films f = new Films()
+                {
+                    IdFilm = reader.GetInt32(0),
+                    Titre = reader.GetString(1),
+                    Genre = reader.GetString(2),
+                    Duree = reader.GetInt32(3),
+                    DateSortie = reader.GetDateTime(4),
+                    Synopsis = reader.GetString(5),
+                    Recommandation = reader.GetInt32(6),
+                    Acteur_Nom = reader.GetString(7),
+                    Realisateur_Nom = reader.GetString(8),
+                    Image = reader.GetString(9),
+                    Video = reader.GetString(10)
+                };
+                films.Add(f);
+            }
+            reader.Close();
+            command.Dispose();
+            connection.Close();
+            return films;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
